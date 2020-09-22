fb000432fd062af2013a5ee25b71e55aab9bb9bd
DavidPavel1 <david.pavel748@sccdsb.net>
2020-09-22 14:10:47 -0400

    started the program didn't finsh

--- AboutMe.java
+++ AboutMe.java
@@ -1,24 +1,25 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ * Programmer: David
+ * Date: Tuesday, September 22, 2020
+ * Purpose: To say things about me
  */
 
 public class AboutMe {
     public static void main (String [] args) {
-        // Declaring the variables
-        // TODO: Write the code the declares the variables
-
+        
         // Print the values
-        System.out.println("Hi, my name is " + myName);
+    	final string myName = "David"
+    	int myAge = 16
+    	double myHeightInMetres = 1.87
+      
+    	System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
-        // Change the variables
-        // TODO: Write the code that changes the variables
-
-        // Print the new values
+        int myAge2 = 19
+        double myHeightInMetres2 = 1.91
+        
         System.out.println("Some time passes...");
-        // TODO: Write the last two print statements
-    }
+        System.out.println("I am now " + myAge2 + "years old.");
+    }   System.out.println("I have grown and am 1.91m tall");
 }
