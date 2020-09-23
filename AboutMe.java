92154fe3e929156b43d78dc9f21d1ad653baa0f6
DavidPavel1 <david.pavel748@sccdsb.net>
2020-09-23 08:14:20 -0400

    Update AboutMe.java
    I changed it up

--- AboutMe.java
+++ AboutMe.java
@@ -8,18 +8,19 @@ public class AboutMe {
     public static void main (String [] args) {
         
         // Print the values
-    	final string myName = "David"
-    	int myAge = 16
-    	double myHeightInMetres = 1.87
+    	String myName = "David";
+    	int myAge = 16;
+    	double myHeightInMetres = 1.87;
       
     	System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
-        int myAge2 = 19
-        double myHeightInMetres2 = 1.91
+        int myAge2 = 19;
+        double myHeightInMetres2 = 1.91;
         
         System.out.println("Some time passes...");
-        System.out.println("I am now " + myAge2 + "years old.");
-    }   System.out.println("I have grown and am 1.91m tall");
+        System.out.println("I am now " + myAge2 + " years old.");
+        System.out.println("I have grown " + myHeightInMetres2 + " m tall");
+    }
 }
