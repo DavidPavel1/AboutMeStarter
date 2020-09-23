8acd86207548b8e57761839070a01386c52e4032
DavidPavel1 <david.pavel748@sccdsb.net>
2020-09-23 09:01:57 -0400

    Update AboutMe.java
    changed

--- AboutMe.java
+++ AboutMe.java
@@ -11,18 +11,20 @@ public class AboutMe {
     	String myName = "David";
     	int myAge = 16;
     	double myHeightInMetres = 1.87;
-      
+        
+    	// Printing first part
     	System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
-
+         
+        // Printing values
         myAge = 19;
         myHeightInMetres = 1.91;
         
         int myAge2 = 19;
         double myHeightInMetres2 = 1.91;
         
-        
+        // Printing second part
         System.out.println("Some time passes...");
         System.out.println("I am now " + myAge2 + " years old.");
         System.out.println("I have grown " + myHeightInMetres2 + " m tall");
